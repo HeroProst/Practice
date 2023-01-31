4562eb5609faeb400cb5dd70d51cb0f1c8557b5a
HeroProst <73166224+HeroProst@users.noreply.github.com>
2023-01-31 17:57:57 +0500

    add comments to tasks

--- Practice/Practice/Afanasiev.cs
+++ Practice/Practice/Afanasiev.cs
@@ -10,7 +10,12 @@ namespace Practice
 		}
 
 		public static string Bullet_and_dragons(int bullets, int dragons)
-		{
+		{/**Герой направляется в замок, чтобы выполнить свою миссию. 
+		 Однако ему сообщили, что замок окружен парой мощных драконов! 
+		 Для победы над каждым драконом требуется 2 пули, и наш герой не знает, сколько пуль он должен иметь при себе.
+		 Выживет ли герой, есди возьмёт заданное количество пуль?
+		 Написать ответ "да" или "нет".
+		 **/
 			if (bullets >= dragons * 2)
 				return "да";
 			else
@@ -18,7 +23,11 @@ namespace Practice
 		}
 
 		public static string Empty(int a, int b)
-		{
+		{/**
+		  Даны 2 целых числа, где a < b,
+		  Выведите через запятую все числа, которые расположены в этом промежутке.
+			Если таких нет вывести "empty"
+		  **/
 			if(a + 1 == b) 
 				return "empty";
 			StringBuilder result = new StringBuilder();
@@ -32,6 +41,10 @@ namespace Practice
 
 		public static double Red_square(double a)
 		{
+			/**
+			 * Завершить функцию расчета площади красного квадрата через длину дуги окружности А.
+			 * Полученный результат округлить до 2 знаков после запятой
+			 * **/
 			double cyrcleLength = a * 4;
 			double cyrcleRadius = cyrcleLength / (2 * Math.PI);
 			return Math.Round(Math.Pow(cyrcleRadius, 2),2);
@@ -39,6 +52,10 @@ namespace Practice
 
 		public static bool Anagram(string test, string origins)
 		{
+			/**
+			 * Напишите код, который проверяет являются ли 2 строки анаграммами друг друга.
+			 * Анаграмма - результат перестановки букв слова для получения нового слова.
+			 * **/
 			if(test.Length != origins.Length) 
 				return false;
 			test = test.ToLower();
